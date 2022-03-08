9988667514d871bcfc4e7cf9c78619bf5337d314
nggaaa <73785040+nggaaa@users.noreply.github.com>
2022-03-08 15:54:45 +0700

    Selesai

--- js/script.js
+++ js/script.js
@@ -13,32 +13,11 @@ window.addEventListener("scroll", () => {
   }
 });
 
-const checkPilihJabatan = () => {
-  if ($("#pil_ketua").is(":checked")) {
-    $("#choose_ketua").addClass("active");
-    $("#choose_anggota").removeClass("active");
-    $("#syarat_ketua").show();
-    $("#syarat_anggota").hide();
-    $("#form_ketua").show();
-    $("#form_anggota").hide();
-  }
-  if ($("#pil_anggota").is(":checked")) {
-    $("#choose_anggota").addClass("active");
-    $("#choose_ketua").removeClass("active");
-    $("#syarat_anggota").show();
-    $("#syarat_ketua").hide();
-    $("#form_ketua").hide();
-    $("#form_anggota").show();
-  }
-};
-$('input[name="osis_jabatan"]').change(checkPilihJabatan);
-
 $(document).ready(function () {
-  checkPilihJabatan();
   $("#loading").addClass("hide");
 
   new Typed("#myname", {
-    strings: ["Calon Pengurus OSIS"],
+    strings: ["Telah Ditutup"],
     stringsElement: null,
     // typing speed
     typeSpeed: 30,
@@ -49,15 +28,4 @@ $(document).ready(function () {
     showCursor: true,
     contentType: "html",
   });
-});
-
-$("#choose_ketua").click(function () {
-  $("#pil_ketua").prop("checked", true);
-  $("#pil_anggota").prop("checked", false);
-  checkPilihJabatan();
-});
-$("#choose_anggota").click(function () {
-  $("#pil_anggota").prop("checked", true);
-  $("#pil_ketua").prop("checked", false);
-  checkPilihJabatan();
 });
